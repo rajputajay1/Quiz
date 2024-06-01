f23e4c98285f80fd01026615df78e123f792eb2b
Lakshay <tech@lawctopus.com>
2024-06-02 03:15:59 +0530

    quiz question

--- src/Pages/QuizQuestionsContainer/QuizQuestionsContainer.jsx
+++ src/Pages/QuizQuestionsContainer/QuizQuestionsContainer.jsx
@@ -1,58 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import QuestionHeader from "../../Component/QuizQuestionsComponent/QuestionHeader/index";
 import QuestionText from "../../Component/QuizQuestionsComponent/QuestionText/index";
 import OptionsContainer from "../../Component/QuizQuestionsComponent/OptionsContainer/index";
 import NextButton from "../../Component/QuizQuestionsComponent/NextButton/index";
 import "./QuizQuestionsContainer.css";
+import { fetchGetData } from "../../api";
 
 const QuizContainer = () => {
-  const [questions] = useState([
-    {
-      id: 1,
-      text: "Your question text comes here, it's a sample text.",
-      options: [
-        { type: "text", content: "Optiodecdjencjnedjcndejcnjdncjdncjdncjndjcndjn 1" },
-        { type: "text", content: "Option 2" },
-        { type: "text", content: "Option 3" },
-        { type: "text", content: "Option 4" },
-      ],
-      hasTimer: false,
-      timerDuration: 10,
-    },
-    {
-      id: 2,
-      text: "Another question text here.",
-      options: [
-        { type: "image", content: "./2.jpg" },
-        { type: "image", content: "./2.jpg" },
-        { type: "image", content: "./2.jpg" },
-        { type: "image", content: "./2.jpg" },
-      ],
-      hasTimer: false,
-    },
-    {
-      id: 3,
-      text: "Yet another question with text and image options.",
-      options: [
-        { type: "text", content: "Option 1" },
-        { type: "image", content: "./3.jpg" },
-        { type: "text", content: "Option 3" },
-        { type: "image", content: "./4.jpg" },
-      ],
-      hasTimer: true,
-      timerDuration: 15,
-    },
-  ]);
-
+  const [quizDetail, setQuizDetail] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [timer, setTimer] = useState(null);
   const [selectedOption, setSelectedOption] = useState(null);
   const [allQuestionsAnswered, setAllQuestionsAnswered] = useState(false);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const fetchQuizData = async () => {
+      try {
+        const quizDetail = await fetchGetData("/quiz/665b927982fad7e0e7dc24ac");
+        setQuizDetail(quizDetail.data);
+        setQuestions(quizDetail.data[0].questions); // Assuming questions are part of quizDetail.data
+        console.log(quizDetail.data); //
+        setLoading(false);
+        toast.success("Quiz data loaded successfully!");
+      } catch (error) {
+        setLoading(false);
+        toast.error("Failed to load quiz data.");
+      }
+    };
+    fetchQuizData();
+  }, []);
 
   useEffect(() => {
     const currentQuestion = questions[currentQuestionIndex];
-    if (currentQuestion.hasTimer) {
-      setTimer(currentQuestion.timerDuration);
+    console.log(currentQuestion);
+    if (currentQuestion?.timer) {
+      setTimer(currentQuestion.timer);
     } else {
       setTimer(null);
     }
@@ -72,6 +59,14 @@ const QuizContainer = () => {
   }, [timer]);
 
   const handleNext = () => {
+
+    console.log(score)
+
+    if (selectedOption !== null) {
+      if (selectedOption === questions[currentQuestionIndex].correctOption) {
+        setScore(score + 1);
+      }
+    }
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
       setSelectedOption(null);
@@ -84,21 +79,26 @@ const QuizContainer = () => {
     setSelectedOption(index);
   };
 
-  if (allQuestionsAnswered) {
-    return <div className="results">All questions answered. Thank you!</div>;
+  if (loading) {
+    return <div className="loader">Loading...</div>;
   }
 
-  const { text, options } = questions[currentQuestionIndex];
+  if (allQuestionsAnswered) {
+    return <div className="results">All questions answered. Thank you!{score}</div>;
+  }
+
+  const { name, options } = questions[currentQuestionIndex] || {};
 
   return (
     <div className="quiz-container">
+      {/* <ToastContainer /> */}
       <div className="sub-container">
         <QuestionHeader
           currentQuestionIndex={currentQuestionIndex}
           totalQuestions={questions.length}
           timer={timer}
         />
-        <QuestionText text={text} />
+        <QuestionText text={name} />
         <OptionsContainer
           options={options}
           selectedOption={selectedOption}
