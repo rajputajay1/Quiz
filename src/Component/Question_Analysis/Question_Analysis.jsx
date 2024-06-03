027986f060c38bdeaea43844255b592e28ad7026
Rohit Soni <rohitsoniahsi001@gmail.com>
2024-06-03 12:48:32 +0530

    question anlysis updated

--- src/Component/Question_Analysis/Question_Analysis.jsx
+++ src/Component/Question_Analysis/Question_Analysis.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import "./Question_Analysis.css";
 import Sidebar from '../sidebar/Sidebar';
@@ -8,21 +9,29 @@ const questionsData = [
         attempted: 60,
         correct: 38,
         incorrect: 22,
+        options: [
+            { optionText: "Option A", count: 20 },
+            { optionText: "Option B", count: 40 }
+        ]
     },
     {
         question: "Q.2 Another question place holder for analysis?",
         attempted: 45,
         correct: 30,
         incorrect: 15,
+        options: [
+            { optionText: "Option A", count: 10 },
+            { optionText: "Option B", count: 25 },
+            { optionText: "Option C", count: 10 }
+        ]
     },
     // Add more questions as needed
 ];
 
-const Question_Analysis = () => {
+const Question_Analysis = ({ quizType }) => {
     return (
         <div className='questionAnalysisWidth'>
         <div className='background_color'>
-
             <div className='quiz_2_questions'>
                 <p className='quiz_Questions_heading'>Quiz 2 Question Analysis</p>
                 <div className='quiz_2_created'>
@@ -34,24 +43,35 @@ const Question_Analysis = () => {
                 {questionsData.map((questionData, index) => (
                     <div key={index} className='question_block'>
                         <p className='all_questions'>{questionData.question}</p>
-                        <div className='answers'>
-                            <div className='first_question'>
-                                <p className='attempted'>{questionData.attempted}</p>
-                                <p className='people_attempt'>people Attempted the question</p>
+                        {quizType === 'q&a' ? (
+                            <div className='answers'>
+                                <div className='first_question'>
+                                    <p className='attempted'>{questionData.attempted}</p>
+                                    <p className='people_attempt'>people Attempted the question</p>
+                                </div>
+                                <div className='first_question'>
+                                    <p className='attempted'>{questionData.correct}</p>
+                                    <p className='people_attempt'>people Answered Correctly</p>
+                                </div>
+                                <div className='first_question'>
+                                    <p className='attempted'>{questionData.incorrect}</p>
+                                    <p className='people_attempt'>people Answered Incorrectly</p>
+                                </div>
                             </div>
-                            <div className='first_question'>
-                                <p className='attempted'>{questionData.correct}</p>
-                                <p className='people_attempt'>people Answered Correctly</p>
+                        ) : (
+                           
+                            <div className='answers'>
+                                {questionData.options.map((option, optIndex) => (
+                                    <div key={optIndex} className='pollQuestion'>
+                                        <p className='attempted'>{option.count}</p>
+                                        <p className='people_attempt'>{option.optionText}</p>
+                                    </div>
+                                ))}
                             </div>
-                            <div className='first_question'>
-                                <p className='attempted'>{questionData.incorrect}</p>
-                                <p className='people_attempt'>people Answered Incorrectly</p>
-                            </div>
-                        </div>
+                        )}
                         <br />
                         <hr />
                     </div>
-                    
                 ))}
             </div>
         </div>
@@ -60,102 +80,3 @@ const Question_Analysis = () => {
 };
 
 export default Question_Analysis;
-
-// import React from 'react';
-// import "./Question_Analysis.css";
-// import Sidebar from '../sidebar/Sidebar';
-
-// const Question_Analysis = ({ type }) => {
-//     // Data for quiz questions
-//     const quizQuestions = [
-//         {
-//             question: "Q.1 Question place holder for analysis?",
-//             attempted: 60,
-//             correct: 38,
-//             incorrect: 22,
-//         },
-//         // Add more quiz questions as needed
-//     ];
-
-//     // Data for poll questions
-//     const pollQuestions = [
-//         {
-//             question: "Poll Question 1?",
-//             options: [
-//                 { option: "Option 1", answered: 15 },
-//                 { option: "Option 2", answered: 20 },
-//                 // Add more options as needed
-//             ]
-//         },
-//         // Add more poll questions as needed
-//     ];
-
-//     const renderQuizAnalysis = () => {
-//         return (
-//             <div className='questions_quiz_2'>
-//                 {quizQuestions.map((questionData, index) => (
-//                     <div key={index} className='question_block'>
-//                         <p className='all_questions'>{questionData.question}</p>
-//                         <div className='answers'>
-//                             <div className='first_question'>
-//                                 <p className='attempted'>{questionData.attempted}</p>
-//                                 <p className='people_attempt'>people Attempted the question</p>
-//                             </div>
-//                             <div className='first_question'>
-//                                 <p className='attempted'>{questionData.correct}</p>
-//                                 <p className='people_attempt'>people Answered Correctly</p>
-//                             </div>
-//                             <div className='first_question'>
-//                                 <p className='attempted'>{questionData.incorrect}</p>
-//                                 <p className='people_attempt'>people Answered Incorrectly</p>
-//                             </div>
-//                         </div>
-//                         <br />
-//                         <hr />
-//                     </div>
-//                 ))}
-//             </div>
-//         );
-//     };
-
-//     const renderPollAnalysis = () => {
-//         return (
-//             <div className='questions_poll'>
-//                 {pollQuestions.map((questionData, index) => (
-//                     <div key={index} className='question_block'>
-//                         <p className='all_questions'>{questionData.question}</p>
-//                         <div className='answers'>
-//                             {questionData.options.map((option, index) => (
-//                                 <div key={index} className='first_question'>
-//                                     <p className='attempted'>{option.answered}</p>
-//                                     <p className='people_attempt'>people Answered {option.option}</p>
-//                                 </div>
-//                             ))}
-//                         </div>
-//                         <br />
-//                         <hr />
-//                     </div>
-//                 ))}
-//             </div>
-//         );
-//     };
-
-//     return (
-//         <div className='questionAnalysisWidth'>
-//             <div className='background_color'>
-//                 <div className='quiz_2_questions'>
-//                     <p className='quiz_Questions_heading'>{type === 'quiz' ? 'Quiz' : 'Poll'} Question Analysis</p>
-//                     <div className='quiz_2_created'>
-//                         <p className='imp'>Created on: 04 Sep, 2023</p>
-//                         <p className='imp1'>Impressions: 667</p>
-//                     </div>
-//                 </div>
-//                 {type === 'quiz' && renderQuizAnalysis()}
-//                 {type === 'poll' && renderPollAnalysis()}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Question_Analysis;
-
