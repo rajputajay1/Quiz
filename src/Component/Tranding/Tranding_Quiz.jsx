467e21ec02ce8e629e759b182778f83af66e486a
ajayrajuput <120632665+ajayrajuput@users.noreply.github.com>
2024-06-03 22:30:20 +0530

    tranding quiz api used

--- src/Component/Tranding/Tranding_Quiz.jsx
+++ src/Component/Tranding/Tranding_Quiz.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import "./Tranding_Quiz.css";
 
-const Tranding_Quiz = ({quizzes}) => {
- 
+const Tranding_Quiz = ({ quizzes }) => {
+
+  console.log("ajay", quizzes);
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-US', options);
+  };
 
   return (
     <>
       <div className='ajay'>
 
-     
-      <p className='tranding_quiz'>Trending Quizzes</p>
-      <div className='grid_quiz'>
-        {quizzes.map(quiz => (
-          <div className='main_quizdiv' key={quiz.id}>
-            <div className='allQuiz_div'>
-              <p className='Quiz_text'>{quiz.name}</p>
-              <p className='quiz_numeric'>
-                {quiz.views} <span><img src="./eye.svg" alt="views" /></span>
-              </p>
+
+        <p className='tranding_quiz'>Trending Quizzes</p>
+        <div className='grid_quiz'>
+          {quizzes.map(quiz => (
+            <div className='main_quizdiv' key={quiz.
+              user_id
+              }>
+              <div className='allQuiz_div'>
+                <p className='Quiz_text'>{quiz.quiz_name}</p>
+                <p className='quiz_numeric'>
+                  {quiz.impression} <span><img src="./eye.svg" alt="views" /></span>
+                </p>
+              </div>
+              <p className='created_date'>Created on: {formatDate(quiz.updatedAt)}</p>
             </div>
-            <p className='created_date'>Created on: {quiz.createdDate}</p>
-          </div>
-        ))}
+          ))}
         </div>
-        </div>
+      </div>
     </>
   );
 };
